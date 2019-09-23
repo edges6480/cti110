72602c56480be4c30b13a260ccc983f903a12a0b
edges6480 <55710360+edges6480@users.noreply.github.com>
2019-09-23 14:26:27 -0400

    Add files via upload

---
+++ P2HW1_MeatTipTax_SarahEdge.py
@@ -0,0 +1,13 @@
+ # program that calculates the total amount of a meal purchased at a restaurant
+    # 9/19/19
+    # CTI-110 P2HW1 - Meal Tip Tax Calculator
+    # Sarah Edge
+    #
+print('Enter charge for food ')
+num1 = float(input('charge for food '))
+print('Enter 15% tip ')
+num2 = float(input('15% Tip '))
+print('Enter 6% Tax')
+num3 = float(input('6% Tax '))
+sum = num1+num2+num3
+print(sum)
